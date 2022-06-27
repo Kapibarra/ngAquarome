30508406836706af3236116f1957c8bc3907168c
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-06-27 16:38:54 +0300

    fixes

--- src/app/homepage/shared/components/hero/hero.component.ts
+++ src/app/homepage/shared/components/hero/hero.component.ts
@@ -18,7 +18,7 @@ export class HeroComponent implements OnInit {
     pagination: { clickable: true },
     scrollbar: { draggable: true },
     autoplay: {
-      delay: 3500,
+      delay: 3333500,
       disableOnInteraction: false
     },
   };

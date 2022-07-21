c72a4055f41a72148f86d8cdf63cfcfe4d4756cf
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-07-22 01:04:38 +0300

    reviews ng for fix

--- src/app/homepage/shared/components/reviews/reviews.component.ts
+++ src/app/homepage/shared/components/reviews/reviews.component.ts
@@ -1,13 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-
+interface Reviews {
+  photo: string;
+  id? : number,
+  name: string;
+  client: string;
+}
 @Component({
   selector: 'app-reviews',
   templateUrl: './reviews.component.html',
   styleUrls: ['./reviews.component.scss']
 })
 export class ReviewsComponent implements OnInit {
+Review: Reviews[] = [{
+  photo: 'string',
+  id : 1,
+  name: 'Петр Сергеевич',
+  client: "Клиент",
+},
+{
+  photo: 'string',
+  id : 1,
+  name: 'Петр Сергеевич',
+  client: "Клиент",
+},
+{
+  photo: 'string',
+  id : 1,
+  name: 'Петр Сергеевич',
+  client: "Клиент",
+},
+{
+  photo: 'string',
+  id : 1,
+  name: 'Петр Сергеевич',
+  client: "Клиент",
+},
+]
 
-  constructor() { }
+  constructor() { 
+
+
+  }
 
   ngOnInit(): void {
   }

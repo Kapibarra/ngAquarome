dfdd6b5f71681f73701cf947f6c2df9ecb3f5ecc
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-08-29 16:03:58 +0300

    changes before UPDATE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,27 @@
-
+// Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { SwiperModule } from 'swiper/angular';
 import { MatDialogModule } from '@angular/material/dialog';
 import { AppRoutingModule } from './app-routing.module';
+import { GalleryModule } from  'ng-gallery';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { LightboxModule } from 'ng-gallery/lightbox';
+// Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './homepage/shared/components/header/header.component';
 import { HeroComponent } from './homepage/shared/components/hero/hero.component';
 import { MainpageComponent } from './homepage/mainpage/mainpage.component';
 import { ButtonMainComponent } from './homepage/shared/components/button-main/button-main.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RequestDialogComponentComponent } from './homepage/shared/components/request-dialog-component/request-dialog-component.component';
 import { ButtonSecondaryComponent } from './homepage/shared/components/button-secondary/button-secondary.component';
 import { ReasonsComponent } from './homepage/shared/components/reasons/reasons.component';
 import { SpecialoffersComponent } from './homepage/shared/components/specialoffers/specialoffers.component';
 import { ServicesComponent } from './homepage/shared/components/services/services.component';
-import { HttpClientModule } from '@angular/common/http';
 import { PricepageComponent } from './homepage/pricepage/pricepage.component';
 import { SpecialofferformComponent } from './homepage/shared/components/specialofferform/specialofferform.component';
-import { FormsModule } from '@angular/forms';
 import { ReviewsComponent } from './homepage/shared/components/reviews/reviews.component';
 import { AboutComponent } from './homepage/shared/components/about/about.component';
 import { ContactspageComponent } from './homepage/contactspage/contactspage.component';
@@ -61,6 +64,10 @@ import { GalleryComponent } from './homepage/shared/components/gallery/gallery.c
     MatDialogModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    GalleryModule.withConfig({
+      // thumbView: 'contain',
+    }),
+    LightboxModule
   ],
   exports: [
   ],
